0864b131909c30a0b27c40c8f12dad243423f90e
Lawrence L <Lawrence.Lee@alumni.ucsd.edu>
2016-04-05 17:01:16 -0700

    Implement shake detection functionality.

--- app/src/main/java/com/example/lawrence/magic8ballfortuneteller/MainActivity.java
+++ app/src/main/java/com/example/lawrence/magic8ballfortuneteller/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.lawrence.magic8ballfortuneteller;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private Button mAskButton;
 
     private Fortune mFortune = new Fortune();
+
+    // for shake detection
+    private double acceleration;
+    private double currentAcceleration;
+    private double lastAcceleration;
+    // value used to determine whether user shook the device to erase
+    private static final int ACCELERATION_THRESHOLD = 100000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +39,55 @@ public class MainActivity extends AppCompatActivity {
         mQuestion = (EditText) findViewById(R.id.userInputEditText);
         mAskButton = (Button) findViewById(R.id.fortuneButton);
 
-        // click listener for ask button
+        // click listener for ask button (it's kind of redundant now with the enter button on soft keyboard)
         mAskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 displayFortune();
-                clearQuestion();
             }
         });
 
+        // key listener for when user presses "Enter" on soft keyboard
+        mQuestion.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                // If the event is a key-down event on the "enter" button
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                    // Perform action on key press
+                    displayFortune();
+
+                    // hide keyboard
+                    InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    in.hideSoftInputFromWindow(mQuestion.getApplicationWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // detect shake events
+        SensorManager sensorManager =
+                (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+
+        // register to listen for accelerometer events
+        sensorManager.registerListener(
+                sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
-    private void displayFortune(){
+    private void displayFortune() {
         String input = null;
 
         // check if user input is null.
         // null.toString might be null if user didn't input anything. hence the try-catch.
         try {
             input = mQuestion.getText().toString().replaceAll("\\s+", "");  // get input, trim all whitespace.
-        } catch (Exception e){
+        } catch (Exception e) {
             mAnswer.setText("You did not enter any input.");
         }
 
@@ -55,16 +97,61 @@ public class MainActivity extends AppCompatActivity {
             mAnswer.setText("You did not ask a question.");
         } else {
         */
-            String f = mFortune.getFortune();
-            mAnswer.setText(f);
-            //mAnswer.setTextColor(f.getColor());
+        String f = mFortune.getFortune();
+        mAnswer.setText(f);
+        //mAnswer.setTextColor(f.getColor());
         /*
         }
         */
 
+        // clear EditText for next question.
+        clearQuestion();
     }
 
-    private void clearQuestion(){
+    private void clearQuestion() {
         mQuestion.setText("");
     }
+
+    // event handler for accelerometer events
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+
+        // use accelerometer to determine whether user shook device
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            long lastUpdate = 0;
+            long curTime = System.currentTimeMillis();
+
+            // only allow one shake detect every half a second (500 millisecs).
+            if ( (curTime - lastUpdate) > 500) {
+                lastUpdate = curTime;
+                curTime = System.currentTimeMillis();
+
+                // get x, y, and z values for the SensorEvent
+                double x = event.values[0];
+                double y = event.values[1];
+                double z = event.values[2];
+
+                // save previous acceleration value
+                lastAcceleration = currentAcceleration;
+
+                // calculate the current acceleration
+                currentAcceleration = x * x + y * y + z * z;
+
+                // calculate the change in acceleration
+                acceleration = currentAcceleration * (currentAcceleration - lastAcceleration);
+
+                // if the acceleration is above a certain threshold
+                if (acceleration > ACCELERATION_THRESHOLD) {
+                    displayFortune();
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // not used
+            // required method of interface SensorEventListener
+        }
+    };
+
 }
